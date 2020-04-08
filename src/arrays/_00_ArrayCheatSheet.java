14aee7f80a97244d16c74f20650e37843e26f8d2
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-04-08 11:32:09 -0700

    ArrayCheatSheet, RobotRace

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,56 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] arrayOfStrings = new String[5];
+		arrayOfStrings[0] = "me1";
+		arrayOfStrings[1] = "me2";
+		arrayOfStrings[2] = "me3";
+		arrayOfStrings[3] = "me4";
+		arrayOfStrings[4] = "me5";
 		//2. print the third element in the array
-		
+		System.out.println(arrayOfStrings[2]);
 		//3. set the third element to a different value
-		
+		arrayOfStrings[2] = "you";
 		//4. print the third element again
-		
+		System.out.println(arrayOfStrings[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int i =0;i<arrayOfStrings.length; i++) {
+			System.out.print(arrayOfStrings[i]+" ");
+		}
+		System.out.println("");
 		
 		//6. make an array of 50 integers
-		
+		int fiftyInts[] = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for(int i =0;i<fiftyInts.length; i++) {
+			Random r = new Random();
+			int rando = r.nextInt(51);
+			fiftyInts[i]=rando;
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		int smallBoi =50;
+		for(int i =0;i<fiftyInts.length; i++) {
+			if(fiftyInts[i]<smallBoi) {
+				smallBoi = fiftyInts[i];
+			}
+		}
+		System.out.println("smallest number: "+smallBoi);
 		//9 print the entire array to see if step 8 was correct
-		
+		for(int i =0;i<fiftyInts.length; i++) {
+	System.out.println( fiftyInts[i]);
+		}
 		//10. print the largest number in the array.
+		int bigBoi =0;
+		for(int i =0;i<fiftyInts.length; i++) {
+			if(fiftyInts[i]>bigBoi) {
+				bigBoi = fiftyInts[i];
+			}
+		}
+		System.out.println("Biggest Number: "+bigBoi);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,17 +1,49 @@
 package arrays;
 
+import java.awt.AWTException;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) throws AWTException {
+	
 
 	//2. create an array of 5 robots.
-
+Robot[] arrayORobots = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+for(int i = 0; i < arrayORobots.length; i++) {
+	arrayORobots[i] = new Robot();
+	arrayORobots[i].moveTo(100+i*100,500);
+	arrayORobots[i].setSpeed(100);
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
-
+int robotPosition1 = 500;
+int robotPosition2 = 500;
+int robotPosition3 =500;
+int robotPosition4 = 500;
+int robotPosition5 = 500;
+while(robotPosition1 != 0 && robotPosition2 != 0 && robotPosition3 != 0 && robotPosition4 != 0 && robotPosition5 != 0 ) {
+for(int i = 0; i < arrayORobots.length; i++) {
+	Random random = new Random();
+	int r = random.nextInt(50);
+	arrayORobots[i].move(r);
+	 robotPosition1 = arrayORobots[0].getY();
+	 robotPosition2 = arrayORobots[1].getY();
+	 robotPosition3 = arrayORobots[2].getY();
+	 robotPosition4 = arrayORobots[3].getY();
+	 robotPosition5 = arrayORobots[4].getY();
+	 if(arrayORobots[i].getY() < 10) {
+		 JOptionPane.showMessageDialog(null, "Robot "+i+1 +" wins!");
+		 break;
+	 }
+}}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 	//7. declare that robot the winner and throw it a party!
@@ -19,4 +51,4 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
-}
+}}
